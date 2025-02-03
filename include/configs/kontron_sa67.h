777eec1f9e1c8304f6c3397f9a9fd2d9303e26b4
Michael Walle <mwalle@kernel.org>
2025-02-03 11:58:39 +0100

    sa67: initial commit
    Signed-off-by: Michael Walle <mwalle@kernel.org>

---
+++ include/configs/kontron_sa67.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration header file for Kontron SMARC-sAM67 board
+ *
+ * Copyright (c) 2025 Kontron Europe GmbH
+ */
+
+#ifndef __CONFIG_SA67_H
+#define __CONFIG_SA67_H
+
+#include <configs/ti_armv7_common.h>
+
+#endif /* __CONFIG_SA67_H */
