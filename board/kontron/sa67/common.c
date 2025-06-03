8df7880be057d296eecd5f642de938749ce245a3
Heiko Thiery <heiko.thiery@gmail.com>
2025-06-03 08:02:24 +0200

    sa67: add spl_board_loader_name support
    To display the correct boot_device name instead of MMC1/MMC2.
    
    Signed-off-by: Heiko Thiery <heiko.thiery@gmail.com>

---
+++ board/kontron/sa67/common.c
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Common board specific initialization for Kontron SMARC-sAM67
+ *
+ * Copyright (c) 2025 Kontron Europe GmbH
+ */
+
+#include <asm/arch/hardware.h>
+#include <spl.h>
+
+#define MAIN_DEVSTAT_PRIMARY_SPI_CSEL 0x40
+
+const char *spl_board_loader_name(u32 boot_device)
+{
+	static char name[16];
+	u32 devstat = readl(CTRLMMR_MAIN_DEVSTAT);
+	struct mmc *mmc;
+
+	switch (boot_device) {
+	case BOOT_DEVICE_MMC1:
+		mmc_init_device(0);
+		mmc = find_mmc_device(0);
+		mmc_init(mmc);
+		snprintf(name, sizeof(name), "eMMC (%s)",
+			 emmc_hwpart_names[EXT_CSD_EXTRACT_BOOT_PART(mmc->part_config)]);
+		return name;
+	case BOOT_DEVICE_MMC2:
+		return "SD card (Test mode)";
+	case BOOT_DEVICE_SPI:
+		if (devstat & MAIN_DEVSTAT_PRIMARY_SPI_CSEL)
+			return "External SPI flash";
+		else
+			return "Failsafe SPI flash";
+	case BOOT_DEVICE_DFU:
+		return "DFU";
+	default:
+		return "(unknown)";
+	}
+}
